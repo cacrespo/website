901006b6d61f5ebe421a6da057174512539c5eed
Carlos A. Crespo <lvccrespo@gmail.com>
2024-09-22 15:17:37 -0300

    feat(blog first steps)

--- blog/models.py
+++ blog/models.py
@@ -2,11 +2,17 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+STATUS = (
+    (0,"Draft"),
+    (1,"Publish")
+)
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
     text = models.TextField()
+    status = models.IntegerField(choices=STATUS, default=0)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
