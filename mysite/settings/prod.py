5659f0a4ad224cded67aea54ca081a9137706673
Carlos A. Crespo <lvccrespo@gmail.com>
2024-12-14 19:28:51 -0300

    feat(db): add dj-database-url

--- mysite/settings/prod.py
+++ mysite/settings/prod.py
@@ -1,19 +1,17 @@
 import os
 from .base import *  # noqa
 
+import dj_database_url
+
+
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(",")
 
-# Aca a reemplazar con dj-database-url
 DATABASES = {
-    'default': {
-        'ENGINE': os.environ.get('SQL_ENGINE'),
-        'NAME': os.environ.get('SQL_DATABASE'),
-        'USER': os.environ.get('SQL_USER'),
-        'PASSWORD': os.environ.get('SQL_PASSWORD'),
-        'HOST': os.environ.get('SQL_HOST'),
-        'PORT': os.environ.get('SQL_PORT'),
-    }
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+    ),
 }
