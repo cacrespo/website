654933303980b6c8f4c1f15fcb9fe76383c98b4e
Carlos A. Crespo <lvccrespo@gmail.com>
2024-12-22 05:43:52 -0300

    fix(db): remove sqlite settings

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -21,10 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG", default=0))
 
 
