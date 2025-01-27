1e41aeba9a21fcff65dc3d00fdc28009850de5d8
Carlos A. Crespo <lvccrespo@gmail.com>
2025-01-26 21:58:32 -0300

    add csrf trusted origin

--- mysite/settings/prod.py
+++ mysite/settings/prod.py
@@ -5,5 +5,7 @@ import logfire
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(",")
 
+CSRF_TRUSTED_ORIGINS = ['https://cacrespo.xyz']
+
 logfire.configure()
 logfire.instrument_django()
