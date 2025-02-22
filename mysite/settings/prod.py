75ea39c0c98707056392dea007bd84156ca619fb
Carlos A. Crespo <lvccrespo@gmail.com>
2025-02-22 16:23:26 -0300

    feat: add sentry sdk

--- mysite/settings/prod.py
+++ mysite/settings/prod.py
@@ -2,6 +2,7 @@ import os
 from .base import *  # noqa
 
 import logfire
+import sentry_sdk
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(",")
 
@@ -9,3 +10,12 @@ CSRF_TRUSTED_ORIGINS = ['https://cacrespo.xyz']
 
 logfire.configure()
 logfire.instrument_django()
+
+sentry_sdk.init(
+    dsn=os.environ.get("SENTRY_DSN"),
+    send_default_pii=True,
+    traces_sample_rate=1.0,
+    _experiments={
+        "continuous_profiling_auto_start": True,
+    },
+)
