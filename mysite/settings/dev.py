90776c5887121a4f83130596ebd196396f5908fd
Carlos A. Crespo <lvccrespo@gmail.com>
2025-03-30 18:54:34 -0300

    feat: add django-debug-toolbar for local development

--- mysite/settings/dev.py
+++ mysite/settings/dev.py
@@ -1,8 +1,29 @@
 from .base import *  # noqa
-
+from .base import INSTALLED_APPS, MIDDLEWARE
+import sys
 
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
 
 SECRET_KEY = "foo"
 
 DEBUG = True
+
+# INTERNAL_IPS is not used because Docker changes REMOTE_ADDR on each run.
+# Instead, we directly configure SHOW_TOOLBAR_CALLBACK to determine when
+# the toolbar should be shown.
+#
+# INTERNAL_IPS = ["localhost", "0.0.0.0", "127.0.0.1", "::1"]
+
+# Only enable the toolbar when we're in debug mode and we're
+# not running tests. Django will change DEBUG to be False for
+# tests, so we can't rely on DEBUG alone.
+ENABLE_DEBUG_TOOLBAR = DEBUG and "test" not in sys.argv
+if ENABLE_DEBUG_TOOLBAR:
+    INSTALLED_APPS += [
+        "debug_toolbar",
+    ]
+    MIDDLEWARE += [
+        "debug_toolbar.middleware.DebugToolbarMiddleware",
+    ]
+
+    DEBUG_TOOLBAR_CONFIG = {"SHOW_TOOLBAR_CALLBACK": lambda request: DEBUG}
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,16 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("pages.urls")),
     path("blog/", include("blog.urls")),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar  # type: ignore # noqa
+
+    urlpatterns += debug_toolbar_urls()
