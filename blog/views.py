e66e516caa687e8a99157abd131e0407e5d481f9
Carlos A. Crespo <lvccrespo@gmail.com>
2025-08-31 16:50:41 -0300

    Basic Search Functionality (#54)
    * feat(blog): add search functionality

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,12 @@
+import re
+from itertools import chain
+
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.utils.html import strip_tags
+from django.utils.safestring import mark_safe
+
 from blog.models import Post, Article
 
 
@@ -35,3 +43,71 @@ def blog_article(request):
         "articles": articles,
     }
     return render(request, "blog/article.html", context)
+
+
+def search_results(request):
+    query = request.GET.get("q")
+    processed_results = []
+    if query:
+        post_results = Post.objects.filter(
+            Q(title__icontains=query) | Q(text__icontains=query)
+        )
+        article_results = Article.objects.filter(
+            Q(title__icontains=query) | Q(comment__icontains=query)
+        )
+
+        combined_results = sorted(
+            chain(post_results, article_results),
+            key=lambda instance: instance.created_at,
+            reverse=True,
+        )
+
+        for result in combined_results:
+            if isinstance(result, Post):
+                text_content = result.text
+                url = reverse("blog_post", args=[result.pk])
+                is_external = False
+                categories = result.categories.all()
+                item_type = "Post"
+            elif isinstance(result, Article):
+                text_content = result.comment
+                url = result.link
+                is_external = True
+                categories = None
+                item_type = "Article"
+            else:
+                continue
+
+            text = strip_tags(text_content)
+            position = text.lower().find(query.lower())
+            snippet = ""
+            if position != -1:
+                start = max(0, position - 50)
+                end = min(len(text), position + len(query) + 50)
+                raw_snippet = text[start:end]
+                highlighted_snippet = re.sub(
+                    f"({re.escape(query)})",
+                    r'<mark class="p-0 bg-warning">\1</mark>',
+                    raw_snippet,
+                    flags=re.IGNORECASE,
+                )
+                snippet = mark_safe(f"...{highlighted_snippet}...")
+
+            processed_results.append(
+                {
+                    "type": item_type,
+                    "title": result.title,
+                    "url": url,
+                    "date": result.created_at,
+                    "snippet": snippet,
+                    "is_external": is_external,
+                    "categories": categories,
+                    "text": text_content,
+                }
+            )
+
+    context = {
+        "query": query,
+        "results": processed_results,
+    }
+    return render(request, "blog/search_results.html", context)
