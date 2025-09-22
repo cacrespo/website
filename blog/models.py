c05436b95df7cf83c83fe0dc831b7e5f83103497
Carlos A. Crespo <lvccrespo@gmail.com>
2025-09-21 21:42:40 -0300

    refactor(blog): abstract embedding and search logic

--- blog/models.py
+++ blog/models.py
@@ -20,7 +20,48 @@ class TimeStampedModel(models.Model):
         abstract = True
 
 
-class Post(TimeStampedModel):
+class Embeddable(TimeStampedModel):
+    embedding = VectorField(dimensions=512, editable=False)
+    _content_field = None  # To be defined in subclasses
+
+    class Meta:
+        abstract = True
+
+    def prepare_vector_information(self):
+        if not self._content_field:
+            raise NotImplementedError("Subclasses of Embeddable must define '_content_field'.")
+        content = getattr(self, self._content_field)
+        title_vec = T.encode(self.title)
+        content_vec = T.encode(content)
+        # Normalize embeddings
+        title_vec /= np.linalg.norm(title_vec)
+        content_vec /= np.linalg.norm(content_vec)
+        # Weighted average
+        embedding = 0.7 * title_vec + 0.3 * content_vec
+        embedding /= np.linalg.norm(embedding)
+        return embedding
+
+    def save(self, *args, **kwargs):
+        self.embedding = self.prepare_vector_information()
+        super().save(*args, **kwargs)
+
+    @classmethod
+    def search(cls, q, dmax=0.5):
+        if not cls._content_field:
+            raise NotImplementedError("Subclasses of Embeddable must define '_content_field'.")
+
+        filter_query = Q(title__icontains=q) | Q(**{f"{cls._content_field}__icontains": q})
+        literal_qs = cls.objects.filter(filter_query)
+        semantic_qs = (
+            cls.objects.alias(distance=CosineDistance("embedding", T.encode(q)))
+            .filter(distance__lt=dmax)
+            .order_by("distance")
+        )
+        return literal_qs.union(semantic_qs)
+
+
+class Post(Embeddable):
+    _content_field = "text"
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
@@ -28,7 +69,6 @@ class Post(TimeStampedModel):
     status = models.IntegerField(choices=STATUS, default=0)
     categories = models.ManyToManyField("Category", related_name="posts")
     published_at = models.DateTimeField(blank=True, null=True)
-    embedding = VectorField(dimensions=512, editable=False)
 
     def publish(self):
         self.published_at = timezone.now()
@@ -36,27 +76,6 @@ class Post(TimeStampedModel):
 
     def __str__(self):
         return self.title
-
-    def save(self, *args, **kwargs):
-        title_vec = T.encode(self.title)
-        text_vec = T.encode(self.text)
-        # Normalize embeddings
-        title_vec /= np.linalg.norm(title_vec)
-        text_vec /= np.linalg.norm(text_vec)
-        # Weighted average
-        self.embedding = 0.7 * title_vec + 0.3 * text_vec
-        self.embedding /= np.linalg.norm(self.embedding)
-        super().save(*args, **kwargs)
-
-    @classmethod
-    def search(cls, q, dmax=0.5):
-        literal_qs = cls.objects.filter(Q(title__icontains=q) | Q(text__icontains=q))
-        semantic_qs = (
-            cls.objects.alias(distance=CosineDistance("embedding", T.encode(q)))
-            .filter(distance__lt=dmax)
-            .order_by("distance")
-        )
-        return literal_qs.union(semantic_qs)
 
 
 class Category(models.Model):
@@ -69,36 +88,15 @@ class Category(models.Model):
         return self.name
 
 
-class Article(TimeStampedModel):
+class Article(Embeddable):
+    _content_field = "comment"
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     comment = models.TextField()
     link = models.URLField()
-    embedding = VectorField(dimensions=512, editable=False)
 
     def __str__(self):
         return self.title
-
-    def save(self, *args, **kwargs):
-        title_vec = T.encode(self.title)
-        comment_vec = T.encode(self.comment)
-        # Normalize embeddings
-        title_vec /= np.linalg.norm(title_vec)
-        comment_vec /= np.linalg.norm(comment_vec)
-        # Weighted average
-        self.embedding = 0.7 * title_vec + 0.3 * comment_vec
-        self.embedding /= np.linalg.norm(self.embedding)
-        super().save(*args, **kwargs)
-
-    @classmethod
-    def search(cls, q, dmax=0.5):
-        literal_qs = cls.objects.filter(Q(title__icontains=q) | Q(comment__icontains=q))
-        semantic_qs = (
-            cls.objects.alias(distance=CosineDistance("embedding", T.encode(q)))
-            .filter(distance__lt=dmax)
-            .order_by("distance")
-        )
-        return literal_qs.union(semantic_qs)
 
 
 class Activity(TimeStampedModel):
